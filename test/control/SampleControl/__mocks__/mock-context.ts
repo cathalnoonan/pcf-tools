9afd1c369685e4ae4e95070638b58be33dff9f72
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2024-04-07 13:32:07 +0100

    Include `isNetworkAvailable` property in mock

--- test/control/SampleControl/__mocks__/mock-context.ts
+++ test/control/SampleControl/__mocks__/mock-context.ts
@@ -16,6 +16,7 @@ export class MockContext<T> implements ComponentFramework.Context<T> {
       getClient: jest.fn(),
       getFormFactor: jest.fn(),
       isOffline: jest.fn(),
+      isNetworkAvailable: jest.fn(),
     };
 
     // Canvas apps currently assigns a positive tab-index
