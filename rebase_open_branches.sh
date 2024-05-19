adcd7da039ab9e94be73616d42301288ff8630b1
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2024-05-19 18:58:36 +0100

    Add rebase script (#42)

---
+++ rebase_open_branches.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+git checkout main
+git fetch --all --prune
+git pull
+
+branches=(
+  $(git branch --all | grep -v 'main' | sed 's|  remotes/origin/||')
+)
+for branch in "${branches[@]}"
+do
+  git checkout "${branch}"
+  git rebase main
+  git push --force-with-lease
+  git checkout main
+  git branch -D "${branch}"
+done
